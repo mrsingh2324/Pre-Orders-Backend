39198934099ed54d37b2e57f397d56175c5f18bc
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2023-10-29 11:15:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose"); // Import Mongoose
+const mongoose = require("mongoose");
 const authController = require("./controllers/authController");
 const api = require("./controllers/fetchData");
 const paymentController = require("./controllers/paymentController");
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Remove the MongoDB client connection code and connect using Mongoose
 const MONGODB_URI =
   "mongodb+srv://satyam1232:satyam@cluster0.utflrfz.mongodb.net/?retryWrites=true&w=majority";
 
@@ -28,13 +27,10 @@ dbConnection.on("error", (err) => {
   console.error("Error connecting to MongoDB:", err.message);
 });
 
-// Remove the User model definition here since it is already defined in models/user.js
-
 app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/signup", authController.signUp);
 app.post("/login", authController.login);
 app.use("/payments", paymentController);
-
 
 app.get("/fetch", async (req, res) => {
   try {
@@ -46,14 +42,13 @@ app.get("/fetch", async (req, res) => {
     };
     const apiData = await api.fetchApiData(queryParams);
     res.json(apiData);
-
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-    
 });
 
 const port = 5000;
+
 app.listen(port, () => {
-  console.log("Server started on port", port);
+  console.log(`Server started on port ${port}`);
 });
